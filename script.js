64e2eecc736ab15ef8369a61f88f15c75b57b71f
jovegod <4nnaji@gmail.com>
2026-05-20 21:46:25 +0100

    Add Power BI practical projects

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function sayHello() {
-  alert("Hello from my first website!");
+const contactButton = document.querySelector('.contact-section .button');
+
+if (contactButton) {
+  contactButton.addEventListener('click', () => {
+    console.log('Opening email client for a project inquiry.');
+  });
 }
